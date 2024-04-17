ecf8bd5a723cce1fb01d70e7a7a40c3c10bb8e18
imhwarang <imhwarang4@naver.com>
2024-04-17 16:38:05 +0900

    feat: 구매하기, 홈으로 버튼 추가

--- week2/cart.js
+++ week2/cart.js
@@ -48,15 +48,25 @@ window.onload = function () {
 
     const deleteBtn = deleteCell.querySelector(".deleteBtn");
 
-    deleteBtn.addEventListener('click', () => {
-        const cartItems = JSON.parse(localStorage.getItem('cart'));
-        const updatedCart = cartItems.filter(cartItem => cartItem.id !== item.id)
-        localStorage.setItem('cart', JSON.stringify(updatedCart))
+    deleteBtn.addEventListener("click", () => {
+      const cartItems = JSON.parse(localStorage.getItem("cart"));
+      const updatedCart = cartItems.filter(
+        (cartItem) => cartItem.id !== item.id
+      );
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
 
-        row.remove();
-    })
+      row.remove();
+    });
 
     // 테이블에 행 추가
     tbody.appendChild(row);
   });
 };
+
+function buyBtnClick() {
+    
+}
+
+function goHome() {
+  window.location.href = "index.html";
+}
