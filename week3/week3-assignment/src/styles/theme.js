60ed5ac2478b35c1e52ca3598afec9a7b0deccf2
imhwarang <imhwarang4@naver.com>
2024-04-26 00:42:12 +0900

    init: 초기세팅

---
+++ week3/week3-assignment/src/styles/theme.js
@@ -0,0 +1,17 @@
+const colors = {
+  MainBack: "#e8f2fa",
+  SkyBlue: "#c4e4ff",
+  Pink: "#d20062",
+  Violet: "#d6589f",
+  LightPurple: "#d895da",
+};
+
+// const fonts = {
+
+// }
+
+const theme = {
+  colors,
+};
+
+export default theme;
