c1cfdacefa657a37fb019dae313603a6abad4d63
imhwarang <imhwarang4@naver.com>
2024-04-28 20:27:53 +0900

    feat: 이미지 뒤집기 애니메이션 추가

--- week3/week3-assignment/src/components/Card.jsx
+++ week3/week3-assignment/src/components/Card.jsx
@@ -1,24 +1,45 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function Card({ image, isOpen, isMatched, onClick }) {
-  return <CardWrapper onClick={onClick}>
-  {isOpen || isMatched ? (
-    <img
-      src={image}
-      alt="card"
-      style={{ width: "inherit", height: "inherit" }}
-    />
-  ) : null}
-</CardWrapper>
+  return (
+    <CardWrapper onClick={onClick} isOpen={isOpen} isMatched={isMatched}>
+      <CardBack />
+      <CardFront>
+        <img src={image} alt="card" />
+      </CardFront>
+    </CardWrapper>
+  );
 }
 
 const CardWrapper = styled.div`
-  width: 100px;
-  height: 150px;
-  background-color: #ccc;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 10px;
+  position: relative;
+  width: 20rem;
+  height: 25rem;
+  margin: 1rem;
   cursor: pointer;
+  transform-style: preserve-3d;
+  transition: transform 0.5s ease;
+  transform: ${({ isOpen, isMatched }) =>
+    isOpen || isMatched ? "rotateY(180deg)" : "rotateY(0deg)"};
+`;
+
+const CardBack = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  background-color: gray;
+`;
+
+const CardFront = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  transform: rotateY(180deg);
+  img {
+    width: 100%;
+    height: 100%;
+  }
 `;
